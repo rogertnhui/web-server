51a81575f84ecc94449e0845cc7ab6a7b9088ff9
Roger Hui <roger.hui@gmail.com>
2017-08-23 19:08:36 +0800

    updated languages

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var middleware = {
 app.use(middleware.logger); 
 
 app.get('/about', middleware.requireAuthentication, function(req,res){
-	res.send('about us'); 
+	res.send('about us!!!'); 
 });
 
 
